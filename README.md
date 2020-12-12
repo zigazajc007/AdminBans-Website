7f45a5ef5a68bd2810a277efa0e4ada968d3dd5e
ziga <ziga.zajc007@gmail.com>
2020-12-12 19:37:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# AdminBans-Website
---
+++ settings-864337.php
@@ -0,0 +1,46 @@
+<?php
+
+//Server name
+$server_name = "Server Name";
+
+//Database
+$mysql_host = "localhost";
+$mysql_port = "3306";
+$mysql_database = "AdminBans";
+$mysql_user = "root";
+$mysql_password = "";
+
+// Login data for Admin Bans panel (Please don't use default password)
+$login_username = "admin";
+$login_password = "d45HKmyHkQkNPGNoZxz7Dwz7i";
+// Do you want to use hCaptcha for login?
+$login_hcaptcha = false;
+$login_hcaptcha_sitekey = "Site Key Here";
+$login_hcaptcha_privatekey = "Private Key Here";
+
+//Choose default theme
+$default_theme = "dark";
+
+//How many rows can be shown on the same time?
+$data_limit = 10;
+
+//Discord server link. Example: "https://discord.gg/hUNymXX" or null
+$discord_link = "Your Link Here";
+
+//Server store link. Example: "http://store.rabbitcraft.net" or null
+$store_link = "Your Link Here";
+
+/* Website for player heads:
+
+    Options:
+        - "https://crafatar.com/avatars/{uuid}?size=20"
+        - "http://cravatar.eu/avatar/{uuid}/20.png"
+        - "https://mc-heads.net/avatar/{uuid}/20"
+        - "https://minotar.net/avatar/{uuid}/20"
+        - "https://visage.surgeplay.com/face/20/{uuid}"
+        - null
+
+*/
+$heads_link = "https://crafatar.com/avatars/{uuid}?size=20";
+
+?>

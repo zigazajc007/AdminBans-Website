de9381be12222d585e5ba7d230d3535819e7bbee
Ziga <ziga.zajc007@gmail.com>
2023-04-14 19:36:12 +0200

    Update

--- website/index.php
+++ website/index.php
@@ -196,51 +196,55 @@ $kick_count = Utils::getRowCount('adminbans_kicked_players');
 			<?php
 		  }else if(isset($_GET["page"]) && $_GET["page"] == 'mutes'){ ?>
 
-				<h1 class="animate__animated animate__bounceInDown">Mutes</h1> <?php
+				<h1 class="text-2xl font-medium mb-2 leading-6 tertiaryColor">Mutes</h1>
 
+				<?php
 				$sql = "SELECT * FROM adminbans_muted_players";
 				$limit = "LIMIT " . Settings::$data_limit;
+				$order = (isset($_GET["order"])) ? $_GET["order"] : null;
 
 				if(isset($_GET["player"]) && $_GET["player"] != ''){
 					$sql = $sql . " WHERE username_to = '" . $_GET["player"] . "'";
+				}else if(isset($_GET["moderator"]) && $_GET["moderator"] != ''){
+					$sql = $sql . " WHERE username_from = '" . $_GET["moderator"] . "'";
+				}else if(isset($_GET["server"]) && $_GET["server"] != ''){
+					$sql = $sql . " WHERE server = '" . $_GET["server"] . "'";
 				}
 
-				if(isset($_GET["order"])){
-					switch ($_GET["order"]) {
-						case 'player':
-							$sql = $sql . " ORDER BY username_to " . $limit;
-							break;
-						case 'player_desc':
-							$sql = $sql . " ORDER BY username_to desc " . $limit;
-							break;
-						case 'moderator':
-							$sql = $sql . " ORDER BY username_from " . $limit;
-							break;
-						case 'moderator_desc':
-							$sql = $sql . " ORDER BY username_from desc " . $limit;
-							break;
-						case 'date':
-							$sql = $sql . " ORDER BY created " . $limit;
-							break;
-						case 'date_desc':
-							$sql = $sql . " ORDER BY created desc " . $limit;
-							break;
-						case 'expires':
-							$sql = $sql . " ORDER BY until " . $limit;
-							break;
-						case 'expires_desc':
-							$sql = $sql . " ORDER BY until desc " . $limit;
-							break;
-						case 'server':
-							$sql = $sql . " ORDER BY until " . $limit;
-							break;
-						case 'server_desc':
-							$sql = $sql . " ORDER BY until desc " . $limit;
-							break;
-						default:
-							$sql = $sql . " " . $limit;
-							break;
-					}
+				switch ($order) {
+					case 'player':
+						$sql = $sql . " ORDER BY username_to " . $limit;
+						break;
+					case 'player_desc':
+						$sql = $sql . " ORDER BY username_to desc " . $limit;
+						break;
+					case 'moderator':
+						$sql = $sql . " ORDER BY username_from " . $limit;
+						break;
+					case 'moderator_desc':
+						$sql = $sql . " ORDER BY username_from desc " . $limit;
+						break;
+					case 'date':
+						$sql = $sql . " ORDER BY created " . $limit;
+						break;
+					case 'date_desc':
+						$sql = $sql . " ORDER BY created desc " . $limit;
+						break;
+					case 'expires':
+						$sql = $sql . " ORDER BY until " . $limit;
+						break;
+					case 'expires_desc':
+						$sql = $sql . " ORDER BY until desc " . $limit;
+						break;
+					case 'server':
+						$sql = $sql . " ORDER BY until " . $limit;
+						break;
+					case 'server_desc':
+						$sql = $sql . " ORDER BY until desc " . $limit;
+						break;
+					default:
+						$sql = $sql . " " . $limit;
+						break;
 				}
 
 				$result = Utils::executeQuery($sql);
@@ -251,29 +255,33 @@ $kick_count = Utils::getRowCount('adminbans_kicked_players');
 				<div class="py-2 align-middle inline-block min-w-full px-2">
 				<div class="shadow overflow-hidden border-b passwordsBorderColor rounded-lg">
 				<table class="min-w-full divide-y passwordsBorderColor table-fixed">
-					<tr>
-						<th><a href="/?page=mutes&order=player<?php if(isset($_GET["order"]) && $_GET["order"] == 'player'){ echo "_desc"; } ?>">Player <?php if(isset($_GET["order"]) && $_GET["order"] == 'player'){?> <i class="fas fa-sort-up"></i> <?php }else if(isset($_GET["order"]) && $_GET["order"] == 'player_desc'){ ?> <i class="fas fa-sort-down"></i> <?php } ?></a></th>
-						<th><a href="/?page=mutes&order=moderator<?php if(isset($_GET["order"]) && $_GET["order"] == 'moderator'){ echo "_desc"; } ?>">Moderator <?php if(isset($_GET["order"]) && $_GET["order"] == 'moderator'){?> <i class="fas fa-sort-up"></i> <?php }else if(isset($_GET["order"]) && $_GET["order"] == 'moderator_desc'){ ?> <i class="fas fa-sort-down"></i> <?php } ?></a></th>
-						<th>Reason</th>
-						<th><a href="/?page=mutes&order=date<?php if(isset($_GET["order"]) && $_GET["order"] == 'date'){ echo "_desc"; } ?>">Date <?php if(isset($_GET["order"]) && $_GET["order"] == 'date'){?> <i class="fas fa-sort-up"></i> <?php }else if(isset($_GET["order"]) && $_GET["order"] == 'date_desc'){ ?> <i class="fas fa-sort-down"></i> <?php } ?></a></th>
-						<th><a href="/?page=mutes&order=expires<?php if(isset($_GET["order"]) && $_GET["order"] == 'expires'){ echo "_desc"; } ?>">Expires <?php if(isset($_GET["order"]) && $_GET["order"] == 'expires'){?> <i class="fas fa-sort-up"></i> <?php }else if(isset($_GET["order"]) && $_GET["order"] == 'expires_desc'){ ?> <i class="fas fa-sort-down"></i> <?php } ?></a></th>
-						<th><a href="/?page=mutes&order=expires<?php if(isset($_GET["order"]) && $_GET["order"] == 'server'){ echo "_desc"; } ?>">Server <?php if(isset($_GET["order"]) && $_GET["order"] == 'server'){?> <i class="fas fa-sort-up"></i> <?php }else if(isset($_GET["order"]) && $_GET["order"] == 'server_desc'){ ?> <i class="fas fa-sort-down"></i> <?php } ?></a></th>
-					</tr>
+					<thead class="secondaryBackgroundColor">
+						<tr>
+							<?php
+								echo Utils::generateHeader('mutes', 'player', 'Player', $order);
+								echo Utils::generateHeader('mutes', 'moderator', 'Moderator', $order);
+								echo Utils::generateHeader('mutes', 'reason', 'Reason', $order, 0);
+								echo Utils::generateHeader('mutes', 'date', 'Date', $order);
+								echo Utils::generateHeader('mutes', 'expires', 'Expires', $order);
+								echo Utils::generateHeader('mutes', 'server', 'Server', $order);
+							?>
+						</tr>
+					</thead>
+					<tbody class="secondaryBackgroundColor divide-y passwordsBorderColor">
 				<?php
-
-
-				for($i = 0; $i < count($result); $i++){
-				?><tr>
-						<td><?php if(Settings::$heads_link != null){ ?><img src="<?php echo str_replace("{name}", $result[$i]['username_to'], Settings::$heads_link); ?>" /><?php } echo " " . $result[$i]['username_to']; ?></td>
-						<td><?php if(Settings::$heads_link != null){ ?><img src="<?php echo str_replace("{name}", $result[$i]['username_from'], Settings::$heads_link); ?>" /><?php } echo " " . $result[$i]['username_from']; ?></td>
-						<td><?php echo Utils::chatColor($result[$i]['reason']); ?></td>
-						<td><?php echo $result[$i]['created']; ?></td>
-						<td><?php echo $result[$i]['until']; ?></td>
-						<td><?php if($result[$i]['server'] != ""){ echo $result[$i]['server']; }else{ echo "-"; } ?></td>
-					</tr>
-					<?php
-				}?>
-			</table>
+						for($i = 0; $i < count($result); $i++){
+							?><tr class='passwordsBorderColor'>
+								<td class="tertiaryColor py-4 px-4"><a href="/?page=bans&player=<?= $result[$i]['username_to'] ?>"><div class="flex justify-start space-x-3"><?php if(Settings::$heads_link != null){ ?><img src="<?php echo str_replace("{name}", $result[$i]['username_to'], Settings::$heads_link); ?>" /><?php } echo "<span class='mt-auto mb-auto'>" . $result[$i]['username_to']; ?></span></div></a></td>
+								<td class="tertiaryColor py-4 px-4"><a href="/?page=bans&moderator=<?= $result[$i]['username_from'] ?>"><div class="flex justify-start space-x-3"><?php if(Settings::$heads_link != null){ ?><img src="<?php echo str_replace("{name}", $result[$i]['username_from'], Settings::$heads_link); ?>" /><?php } echo "<span class='mt-auto mb-auto'>" . $result[$i]['username_from']; ?></span></div></a></td>
+								<td class="tertiaryColor py-4 px-4"><?php if($result[$i]['reason'] != null) echo Utils::chatColor($result[$i]['reason']); ?></td>
+								<td class="tertiaryColor py-4 px-4 whitespace-nowrap"><?php echo $result[$i]['created']; ?></td>
+								<td class="tertiaryColor py-4 px-4 whitespace-nowrap"><?php if($result[$i]['until'] == '9999-12-31 23:59:59'){ echo 'Never'; }else{ echo $result[$i]['until']; } ?></td>
+								<td class="tertiaryColor py-4 px-4 whitespace-nowrap"><a href="/?page=bans&server=<?= $result[$i]['server'] ?>"><?php if($result[$i]['server'] != ""){ echo $result[$i]['server']; }else{ echo "-"; } ?></a></td>
+							</tr>
+							<?php
+						}?>
+					</tbody>
+				</table>
 			</div>
 			</div>
 			</div>

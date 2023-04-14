641d6f4a9e3d39daa6685bd36d9f51fa8d162e47
Ziga <ziga.zajc007@gmail.com>
2023-04-14 20:09:11 +0200

    Update

--- website/js/index.js
+++ website/js/index.js
@@ -1,5 +1,20 @@
 import Utils from './utils.js';
 
+const parms = new URLSearchParams(window.location.search);
+
 document.getElementById("menu-toggle-btn").addEventListener('click', () => {
 	Utils.toggleMenu();
 });
+
+try{
+	if(parms.get('player') !== null){
+		document.getElementById("search").value = parms.get('player');
+	}
+
+	document.getElementById("search").addEventListener("keypress", (event) => {
+		if (event.key !== "Enter" && event.key !== " ") return;
+		event.preventDefault();
+		let search = document.getElementById("search").value.split(' ')[0];
+		if(search.length !== 0) window.location.href = "/?page=" + parms.get('page') + "&player=" + search;
+	});
+}catch{};
